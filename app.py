12f478a99166ae83b8b5fcad9f539e264a28517d
Ari Toren-Herrinton <atorenherrinton@Aris-MacBook-Pro.local>
2021-04-30 15:10:32 -0700

    fixing authentication error

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.debug = True
 
 # Use a service account
 cred = credentials.Certificate(
-    'sdk/message-fe49a-firebase-adminsdk-pmzac-f9cf5dc281.json')
+    'sdk/message-fe49a-d990cb4a63f4.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
